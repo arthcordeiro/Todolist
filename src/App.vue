f82eafecbee5d4ccc5f7d19767fd309495c9fefe
Souza <arthur.souza@capgemini.com>
2020-01-08 13:59:11 -0300

    todolist vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <todolist />
+  </div>
+</template>
+
+<script>
+import todolist from './components/Todolist.vue'
+
+export default {
+  name: 'app',
+  components: {
+    todolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+</style>
